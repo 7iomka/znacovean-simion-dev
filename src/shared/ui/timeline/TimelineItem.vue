0ffda3e1d1eb7bbf58653461f1999e95b6053b49
7iomka <7iomka@gmail.com>
2024-07-05 01:57:35 +0300

    [Feat] Timeline component

---
+++ src/shared/ui/timeline/TimelineItem.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+  import { cn } from '@/shared/lib/cn';
+  import type { HTMLAttributes } from 'vue';
+
+  const props = defineProps<{
+    class?: HTMLAttributes['class'];
+    icon?: string;
+  }>();
+
+  const slots = defineSlots<{
+    icon: () => any;
+    default(): any;
+  }>();
+</script>
+
+<template>
+  <div :class="cn('timeline-item', props.class)">
+    <div class="timeline-item__icon">
+      <slot name="icon">{{ icon }}</slot>
+    </div>
+    <div class="timeline-item__line"></div>
+    <div class="timeline-item__content"><slot /></div>
+  </div>
+</template>
